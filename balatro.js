57f3c0ee6abfbc3ccf055b113562fb6ef168eb12
gabriel <148803470+thek1llerbadger@users.noreply.github.com>
2025-04-30 16:30:47 -0300

    Add files via upload

--- balatro.js
+++ balatro.js
@@ -1,20 +1,21 @@
-let qtdNotas, i = 0, nota, somaNotas = 0, media;
-let nome = "", acumulaNome = ""
+        let tentativa = 0;
+        let acesso = false;
+        let senha = document.getElementById("senha")
+        let email = document.getElementsByName("Email")
 
-function calcularNota(){
-qtdNotas = parseInt(prompt("Digite a Quantidade de Notas: "))
-while(i<qtdNotas){
-nota = parseFloat(prompt("Digite uma nota: "))
-somaNotas += nota
-i++
-}
-media = somaNotas/qtdNotas
-document.getElementById("resposta").innerHTML = `A sua média é ${media.toFixed(1)}`
-}
+       function iniciarLogin(){
+            while (tentativa < 3 || !acesso) {
+                if (senha == 1234 && email == "marcioherobrine@gmail.com")
+                {
+                    alert("Acesso garantido");
+                    acesso = true;
+                }else{
+                    tentativa++;
+                    alert("Senha incorreta, tente novamente. Tentativas restantes: " + (3 - tentativa));
+                }
+            }
 
-function algunsnomes(){
-while (nome != 'SAIR'){
-    nome = prompt('Digite um Nome ou SAIR:').toUpperCase()
-    acumulaNome += nome + ', '
-    document.getElementById("acumulaNome").innerHTML = `Os nomes digitados foram: ${acumulaNome}`}
-}
+            if (!acesso) {
+                alert("Acesso bloqueado. Número máximo de tentativas excedido.");
+            }
+        }
